4bfe0abcf05ae8d641c1dccd2f2f08bf79ad9d6e
Plamen Krastev <p_krastev@hotmail.com>
2015-12-16 18:17:28 -0500

    Added BASH shell example

---
+++ Languages/BASH/sum.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#=================================================
+# Program: sum.sh
+#          Sum up integers from 1 to N, where N
+#          is read from the command line
+#
+# Example usage: sh sum.sh 100
+#=================================================
+n=$1
+k=0
+for i in `seq 1 $n`
+do
+    k=$(($k+$i))
+done
+echo "Sum of integers from 1 to $n is $k."
