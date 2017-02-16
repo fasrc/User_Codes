50613cb4a5e77e0c9c9ec0bd1a3337c2cf6c3019
Plamen Krastev <p_krastev@hotmail.com>
2017-02-16 11:49:10 -0500

    Added matrix diagonalization example with MKL

---
+++ Example_Recipes/matrix_diag/README.md
@@ -0,0 +1,39 @@
+### Purpose:
+
+Example of using MKL libraries on the cluster. The specific example creates 300X300 random matrix and diagonalizes it.
+The program prints out the first 10 eigen values.
+
+### Contents:
+
+(1) matrix_diag.f90: Fortran source code
+
+(2) Makefile: Makefile to compile the source code
+
+(3) run.sbatch: Btach-job submission script to send the job to the queue
+
+### Example Usage:
+
+	source new-modules.sh
+	module load intel/15.0.0-fasrc01
+	module load intel-mkl/11.0.0.079-fasrc02
+	make
+	sbatch run.sbatch
+    
+NOTE: To change matrix dimension adjust the parameter n (line 11) in matrix_diag.f90 and recompile the code
+by "make clean; make".
+
+### Example Output:
+
+```
+ First M eigen values of h:
+           1  -31.5535401215023     
+           2  -31.3893450362036     
+           3  -31.3287443137241     
+           4  -31.1211986413599     
+           5  -31.0084719275123     
+           6  -30.9443787874679     
+           7  -30.9043033720232     
+           8  -30.8070563099242     
+           9  -30.7319332144362     
+          10  -30.6680361297837
+```
---
+++ Example_Recipes/matrix_diag/matrix_diag.f90
@@ -0,0 +1,65 @@
+!====================================================================
+! Program: matrix_diag.f90
+!
+! Create a random matrix and diagonalize it
+!====================================================================
+program omp_diag
+  use IFPORT
+  implicit none
+  integer(4) :: i
+  integer(4) :: j
+  integer(4), parameter :: n = 3000    ! Matrix dimension ( n x n )
+  integer(4), parameter :: m = 10      ! Number of eigen values to print out
+  integer(4), parameter :: iseed = 546 ! Seed for random number generator
+  real(8), allocatable :: h(:,:)       ! Matrix to diagonalize
+  real(8), allocatable :: eig(:)       ! Array with eigen values of h
+
+! Initialize the random number generator
+  call srand(iseed)
+
+! Allocate memory.....................................................
+  if ( .not. allocated ( h ) ) allocate ( h(n,n) )
+  if ( .not. allocated ( eig ) ) allocate ( eig(n) )
+
+! Create random test matrix h.........................................
+  do j = 1, n
+     do i = 1, j
+        h(i,j) = rand()
+        h(j,i) = h(i,j)
+     end do
+  end do
+
+! Diagonalize the matrix h............................................
+  call diasym(h,eig,n)
+! Write eigen values of h.............................................
+  write(6,*)'First M eigen values of h:'
+  do i = 1, m
+     write(6,*)i,eig(i)
+  end do
+
+! Free memory.........................................................
+  if ( allocated ( h ) ) deallocate ( h )
+  if ( allocated ( eig ) ) deallocate ( eig )
+ 
+  stop
+end program omp_diag
+
+!=====================================================================
+! Call LAPACK diagonalization subroutine DSYEV
+! Input:  a(n,n) = real symmetric matrix to be diagonalized!
+!             n  = size of a
+! Output: a(n,n) = orthonormal eigenvectors of a
+!         eig(n) = eigenvalues of a in ascending order
+!=====================================================================
+ subroutine diasym(h,eig,n)
+   implicit none
+   integer(4) :: n
+   integer(4) :: l
+   integer(4) :: inf
+   real(8)    :: h(n,n)
+   real(8)    :: eig(n)
+   real(8)    :: work(n*(3+n/2))
+   l=n*(3+n/2)
+   call dsyev('V','U',n,h,n,eig,work,l,inf)
+   return
+ end subroutine diasym
