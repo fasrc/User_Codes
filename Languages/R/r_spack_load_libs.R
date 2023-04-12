d4e6c06b8160b2df44601397700f1caec05260b6
paulasanematsu <paula_sanematsu@g.harvard.edu>
2023-04-12 11:09:09 -0400

    R with Spack code example

---
+++ Languages/R/r_spack_load_libs.R
@@ -0,0 +1,15 @@
+# load libraries
+library(raster)
+library(rgdal)
+library(raster)
+library(terra)
+
+# print something
+print("Hello World!")
+
+# print loaded libraries
+print("Loaded libraries:")
+sessionInfo()
+
+# add your code here
+
