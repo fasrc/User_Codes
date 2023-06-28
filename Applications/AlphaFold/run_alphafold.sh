e87e731049e121395be476064058570ed69b9f6c
paulasanematsu <paula_sanematsu@g.harvard.edu>
2023-06-28 10:09:39 -0400

    Update singularity image and database path
      - --mgnify_database_path=/data/mgnify/mgy_clusters_2022_05.fa
      - --uniref30_database_path=/data/uniref30/UniRef30_2021_03
      The first uses the updated mgnify database info. The second
      replaces the --uniclust30 term with --uniref30 (as well as
      specifying a new database)

--- Applications/AlphaFold/run_alphafold.sh
+++ Applications/AlphaFold/run_alphafold.sh
@@ -1,36 +1,59 @@
 #!/bin/bash
-#SBATCH -J alphafold          # Job name
+#SBATCH -J AF_monomer         # Job name
 #SBATCH -p gpu                # Partition(s) (separate with commas if using multiple)
 #SBATCH --gres=gpu:1          # number of GPUs
 #SBATCH -c 8                  # Number of cores
 #SBATCH -t 03:00:00           # Time (D-HH:MM:SS)
 #SBATCH --mem=60G             # Memory
-#SBATCH -o alphafold_%j.out   # Name of standard output file
-#SBATCH -e alphafold_%j.err   # Name of standard error file
+#SBATCH -o AF_mono_%j.out   # Name of standard output file
+#SBATCH -e AF_mono_%j.err   # Name of standard error file
+
+# set fasta file name
+# NOTE: assumes this is in the directory you are running this script in
+# note that you can run multiple proteins _sequentially_ (with the same model type)
+# the names need to be provided as "protein1.fasta,protein2.fasta"
+# if running multimer, provide one multifasta file
+# indicate oligomeric state by including extra copies of a sequence
+# they still require different _names_ though
+my_fasta=5ZE6_1.fasta
+
+# set fasta-specific subfolder and filepath
+# handling different possible .fasta suffixes
+fasta_name="${my_fasta//.fasta}"
+fasta_name="${fasta_name//.faa}"
+fasta_name="${fasta_name//.fa}"
+mkdir -p $fasta_name
+cp $my_fasta $PWD/$fasta_name
+my_fasta_path=$PWD/$fasta_name/$my_fasta
 
 # create and set path of output directory
-my_output_dir=alphafold_output           # name of output directory
-mkdir $my_output_dir                     # create output directory
-my_output_dir_path=$PWD/$my_output_dir   # entire path of output directory
+my_output_dir=output
+mkdir -p $PWD/$fasta_name/$my_output_dir
+my_output_dir_path=$PWD/$fasta_name/$my_output_dir
 
-# set fasta file name and path
-my_fasta=5ZE6_1.fasta
-my_fasta_path=$PWD/$my_fasta
+# set model type (monomer, multimer, monomer_casp14, monomer_ptm)
+# see notes under fasta file if running multimer
+my_model_type=monomer
 
-# run AlphaFold using Singularity
-singularity run --env TF_FORCE_UNIFIED_MEMORY=1,XLA_PYTHON_CLIENT_MEM_FRACTION=4.0,OPENMM_CPU_THREADS=$SLURM_CPUS_PER_TASK,LD_LIBRARY_PATH=/usr/local/cuda-11.1/targets/x86_64-linux/lib/ -B /n/holylfs04-ssd2/LABS/FAS/alphafold_database:/data -B .:/etc --pwd /app/alphafold --nv /n/singularity_images/FAS/alphafold/alphafold_v2.2.4.sif \
-    --data_dir=/data/ \
-    --bfd_database_path=/data/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
-    --db_preset=full_dbs \
-    --fasta_paths=$my_fasta_path \
-    --max_template_date=2021-07-28 \
-    --mgnify_database_path=/data/mgnify/mgy_clusters_2018_12.fa \
-    --model_preset=monomer \
-    --obsolete_pdbs_path=/data/pdb_mmcif/obsolete.dat \
-    --output_dir=$my_output_dir_path/ \
-    --pdb70_database_path=/data/pdb70/pdb70 \
-    --template_mmcif_dir=/data/pdb_mmcif/mmcif_files \
-    --uniclust30_database_path=/data/uniclust30/uniclust30_2018_08/uniclust30_2018_08 \
-    --uniref90_database_path=/data/uniref90/uniref90.fasta \
-    --use_gpu_relax=True
+# max pdb age
+# use if you want to avoid recent templates
+# format yyyy-mm-dd
+my_max_date="2100-01-01"
+
+# run AlphaFold monomer using Singularity
+singularity run --nv --env TF_FORCE_UNIFIED_MEMORY=1,XLA_PYTHON_CLIENT_MEM_FRACTION=4.0,OPENMM_CPU_THREADS=$SLURM_CPUS_PER_TASK,LD_LIBRARY_PATH=/usr/local/cuda-11.1/targets/x86_64-linux/lib/ --bind /n/holylfs04-ssd2/LABS/FAS/alphafold_database:/data -B .:/etc --pwd /app/alphafold /n/singularity_images/FAS/alphafold/alphafold_2.3.1.sif \
+--data_dir=/data/ \
+--bfd_database_path=/data/bfd/bfd_metaclust_clu_complete_id30_c90_final_seq.sorted_opt \
+--db_preset=full_dbs \
+--fasta_paths=$my_fasta_path \
+--max_template_date=$my_max_date \
+--mgnify_database_path=/data/mgnify/mgy_clusters_2022_05.fa \
+--model_preset=$my_model_type \
+--obsolete_pdbs_path=/data/pdb_mmcif/obsolete.dat \
+--output_dir=$my_output_dir_path \
+--pdb70_database_path=/data/pdb70/pdb70 \
+--template_mmcif_dir=/data/pdb_mmcif/mmcif_files \
+--uniref30_database_path=/data/uniref30/UniRef30_2021_03 \
+--uniref90_database_path=/data/uniref90/uniref90.fasta \
+--use_gpu_relax=True
 
