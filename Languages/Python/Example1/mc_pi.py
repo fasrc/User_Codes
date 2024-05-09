7143f542aec7796188b55f89678f950db3197de6
Paul Edmon <pedmon@cfa.harvard.edu>
2024-05-09 14:36:16 -0400

    Making sure this doesn't depend on numpy.

--- Languages/Python/Example1/mc_pi.py
+++ Languages/Python/Example1/mc_pi.py
@@ -3,20 +3,16 @@
 Program: mc_pi.py
          Monte-Carlo calculation of PI
 """
-import numpy as np
-import numpy.random as ran
+import random
 
 N = 100000
 
-pi = np.pi
-
-iseed = 99
-ran.seed(iseed) # Random seed
+pi = 3.1415926535897
 
 count = 0
 for i in range(N):
-    x = ran.rand(1)
-    y = ran.rand(1)
+    x = random.random()
+    y = random.random()
     z = x*x + y*y
     if z <= 1.0:
         count = count + 1
