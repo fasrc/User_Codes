8411697c9b6ff556a17a861c3b8cd4ccd8709538
Austin Swinney <swinney@gmail.com>
2024-10-09 17:20:28 -0400

    UserDoc Migration Updates

---
+++ AI/PyTorch/pytorch_with_cuda.sh
@@ -0,0 +1,18 @@
+# PyTorch with CUDA 11.8 from a software module
+
+# Start an interactive job on a GPU node
+salloc -p gpu -t 0-06:00 --mem=8000 --gres=gpu:1
+
+# Load required modules
+module load python
+module load cuda/11.8.0-fasrc01
+
+# Create a conda environment and activate it
+mamba create -n pt2.2.0_cuda11.8 python=3.10 pip wheel -y
+source activate pt2.2.0_cuda11.8
+
+# Install PyTorch
+mamba install pytorch torchvision torchaudio pytorch-cuda=11.8 -c pytorch -c nvidia
+
+# Install additional packages
+mamba install pandas scikit-learn matplotlib seaborn jupyterlab -y
---
+++ AI/PyTorch/setup_environment.sh
@@ -0,0 +1,21 @@
+# These instructions set up a conda> environment with PyTorch version & CUDA version 
+
+# Start an interactive job requesting GPUs
+salloc -p gpu -t 0-06:00 --mem=8000 --gres=gpu:1
+
+# Load required software modules
+module load python/3.10.13-fasrc01
+# Create a conda environment
+mamba create -n pt2.3.0_cuda12.1 python=3.10 pip wheel
+
+# Activate the conda environment
+source activate pt2.3.0_cuda12.1
+
+# Install CUDA toolkit version 12.1.0
+mamba install -c "nvidia/label/cuda-12.1.0" cuda-toolkit=12.1.0
+
+# Install PyTorch
+mamba install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia
+
+# Install additional Python packages
+mamba install -c conda-forge numpy scipy pandas matplotlib seaborn h5py jupyterlab jupyterlab-spellchecker scikit-learn
